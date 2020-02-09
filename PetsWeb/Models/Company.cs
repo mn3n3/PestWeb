3136dae90da05b4de06921bad9667ca538bcc16c
Mohammed Awad <52838423+MohdAwad@users.noreply.github.com>
2020-02-09 23:41:49 +0200

    add 09022020

---
+++ PetsWeb/Models/Company.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PetsWeb.Models
+{
+    public class Company
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int CompanyID { get; set; }
+        [Display(Name = "ArabicName", ResourceType = typeof(Resources.Resource))]
+        public string ArabicName { get; set; }
+        [Display(Name = "EnglishName", ResourceType = typeof(Resources.Resource))]
+        public string EnglishName { get; set; }
+        [Display(Name = "Website", ResourceType = typeof(Resources.Resource))]
+        public string Website { get; set; }
+        [Display(Name = "Email", ResourceType = typeof(Resources.Resource))]
+        public string Email { get; set; }
+        [Display(Name = "Telephone", ResourceType = typeof(Resources.Resource))]
+        public string Telephone { get; set; }
+        [Display(Name = "Mobile", ResourceType = typeof(Resources.Resource))]
+        public string Mobile { get; set; }
+        [Display(Name = "TeleFax", ResourceType = typeof(Resources.Resource))]
+        public string TeleFax { get; set; }
+        [Display(Name = "ArbAddress", ResourceType = typeof(Resources.Resource))]
+        public string ArabicAddress { get; set; }
+        [Display(Name = "EngAddress", ResourceType = typeof(Resources.Resource))]
+        public string EnglishAddress { get; set; }
+        [Display(Name = "LogoPath", ResourceType = typeof(Resources.Resource))]
+        public string LogoPath { get; set; }
+    }
+}
---
+++ PetsWeb/Models/DetailsOfOwnership.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PetsWeb.Models
+{
+    public class DetailsOfOwnership
+    {
+        public Company Company { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int CompanyID { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        [Display(Name = "Serial", ResourceType = typeof(Resources.Resource))]
+        public int OwnerID { get; set; }
+        [Required]
+        [Display(Name = "Surname", ResourceType = typeof(Resources.Resource))]
+        public string Surname { get; set; }
+        [Required]
+        [Display(Name = "FirstName", ResourceType = typeof(Resources.Resource))]
+        public string FirstName { get; set; }
+        [Display(Name = "Address", ResourceType = typeof(Resources.Resource))]
+        public string Address { get; set; }
+        [Display(Name = "PosCode", ResourceType = typeof(Resources.Resource))]
+        public string PosCode { get; set; }
+        [Display(Name = "Telephone", ResourceType = typeof(Resources.Resource))]
+        public string Telephone { get; set; }
+        [Display(Name = "CountyName", ResourceType = typeof(Resources.Resource))]
+        public int CountryID { get; set; }
+        [Display(Name = "CityName", ResourceType = typeof(Resources.Resource))]
+        public int CityID { get; set; }
+        [Display(Name = "InsUserName", ResourceType = typeof(Resources.Resource))]
+        public string InsUserID { get; set; }
+        [Display(Name = "InsDateTime", ResourceType = typeof(Resources.Resource))]
+        public DateTime InsDateTime { get; set; }
+    }
+}
